458ec7cb5194c1e7c970c252f0aa12b93b5b0b5c
cerv17 <cpascual2005@gmail.com>
2023-06-21 18:22:32 +0200

    upgrades

--- js-etch.js
+++ js-etch.js
@@ -1,5 +1,15 @@
 const body = document.querySelector('body');
+const view = document.querySelector("#view");
+const reset = document.querySelector("#reset");
+reset.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => square.style.backgroundColor = "white");
+});
+function randomRGB(){
+    return "rgb( " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
+}
 function grid(number){
+    view.innerHTML = "";
     for(let i = 0; i < number; i++){
         const container = document.createElement('div');
         container.classList.add('container');
@@ -8,11 +18,11 @@ function grid(number){
             div.classList.add('square');
             container.appendChild(div);
         }
-        body.appendChild(container);
+        view.appendChild(container);
     }
     //testing
     const squares = document.querySelectorAll('.square');
-    squares.forEach((square) => square.addEventListener('mouseover', () => square.style.backgroundColor = 'black'));
+    squares.forEach((square) => square.addEventListener('mouseover', () => square.style.backgroundColor = randomRGB()));
 }
 let number = 0;
 const button = document.querySelector('button');
